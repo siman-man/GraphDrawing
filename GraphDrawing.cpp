2fb79f63782dac6ea14b8c202fce9ef15310521d
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-04-17 08:20:10 +0900

    temp save

--- GraphDrawing.cpp
+++ GraphDrawing.cpp
@@ -37,7 +37,17 @@ double getTime(unsigned long long int begin_cycle) {
     return (double) (getCycle() - begin_cycle) / CYCLE_PER_SEC;
 }
 
-struct Edge {
+typedef struct Coord {
+    int y;
+    int x;
+
+    Coord(int y, int x) {
+        this->y = y;
+        this->x = x;
+    }
+} COORD;
+
+typedef struct Edge {
     int from;
     int to;
     int dist;
@@ -47,21 +57,64 @@ struct Edge {
         this->to = to;
         this->dist = dist;
     }
-};
+} EDGE;
 
-struct Vertex {
+typedef struct Vertex {
     int y;
     int x;
+    double minDiff;
+    double maxDiff;
+    vector<int> neighbors;
 
-    Vertex(int y = -1, int x = -1) {
+    Vertex(int y = -1, int x = -1, double minDiff = -1.0, double maxDiff = -1.0) {
         this->y = y;
         this->x = x;
+        this->minDiff = minDiff;
+        this->maxDiff = maxDiff;
+    }
+
+    double range() {
+        assert(maxDiff - minDiff >= 0);
+        return maxDiff - minDiff;
+    }
+
+} VERTEX;
+
+struct Diff {
+    double min;
+    double max;
+    double range;
+
+    Diff(double minDiff = -1.0, double maxDiff = -1.0, double range = -1.0) {
+        this->min = minDiff;
+        this->max = maxDiff;
+        this->range = range;
     }
 };
 
-vector <Vertex> g_vertexList;
-vector<Edge> g_edgeList;
-bool g_field[FIELD_SIZE+1][FIELD_SIZE+1];
+struct Node {
+    double totalRange;
+    double maxDiff;
+    double minDiff;
+
+    Node(double totalRange = -1.0, double maxDiff = -1.0, double minDiff = -1.0) {
+        this->totalRange = totalRange;
+        this->maxDiff = maxDiff;
+        this->minDiff = minDiff;
+    }
+
+    double value() {
+        return 0.0;
+    }
+
+    double score() {
+        return minDiff / maxDiff;
+    }
+};
+
+vector <VERTEX> g_vertexList;
+vector <Edge> g_edgeList;
+bool g_field[FIELD_SIZE + 1][FIELD_SIZE + 1];
 int g_desireDist[MAX_V][MAX_V];
 int g_edgeCount;
 int g_vertexCount;
@@ -74,85 +127,91 @@ public:
         g_edgeCount = edges.size() / 3;
         g_startCycle = getCycle();
 
+        memset(g_desireDist, -1, sizeof(g_desireDist));
+
+        // initialize vertex
+        for (int i = 0; i < N; ++i) {
+            int y = xor128() % FIELD_SIZE;
+            int x = xor128() % FIELD_SIZE;
+            g_vertexList.push_back(VERTEX(y, x));
+        }
+
         for (int i = 0; i < g_edgeCount; i++) {
             int from = edges[i * 3];
             int to = edges[i * 3 + 1];
             int dist = edges[i * 3 + 2];
+
+            g_desireDist[from][to] = dist;
+            g_desireDist[to][from] = dist;
+
+            g_vertexList[from].neighbors.push_back(to);
+            g_vertexList[to].neighbors.push_back(from);
             g_edgeList.push_back(Edge(from, to, dist));
         }
-
-        memset(g_desireDist, -1, sizeof(g_desireDist));
 
         fprintf(stderr, "VertexCount = %d, EdgeCount = %d\n", g_vertexCount, g_edgeCount);
     }
 
-    void initField(vector<Vertex> vl) {
+    void initFieldData(vector<VERTEX> &vl) {
         memset(g_field, false, sizeof(g_field));
 
         for (int i = 0; i < vl.size(); i++) {
-            Vertex v = vl[i];
-            g_field[v.y][v.x] = true;
+            VERTEX *v = &vl[i];
+            Diff diff = calcVertexDiff(vl, i);
+            v->minDiff = diff.min;
+            v->maxDiff = diff.max;
+            g_field[v->y][v->x] = true;
         }
     }
 
     vector<int> plot(int N, vector<int> edges) {
         init(N, edges);
 
-        for (int i = 0; i < N; ++i) {
-            int y = xor128() % FIELD_SIZE;
-            int x = xor128() % FIELD_SIZE;
-            g_vertexList.push_back(Vertex(y, x));
-        }
-
         vector <Vertex> bvl = getBestVertexList(g_vertexList);
 
         vector<int> ret = createAnswer(bvl);
-        double score = calcScore(bvl);
-        fprintf(stderr, "CurrentScore = %f\n", score);
+        Node node = calcScore(bvl);
+        fprintf(stderr, "CurrentScore = %f\n", node.score());
 
         return ret;
     }
 
     vector <Vertex> getBestVertexList(vector <Vertex> vertexList) {
         vector <Vertex> bestVertexList = vertexList;
-        vector <Vertex> goodVertexList = vertexList;
+        initFieldData(bestVertexList);
+
         double currentTime = getTime(g_startCycle);
-        double bestScore = calcScore(bestVertexList);
-        double goodScore = bestScore;
-        double diffScore;
+        Node node = calcScore(bestVertexList);
+        double bestValue = node.totalRange;
 
-        initField(bestVertexList);
-
-        int R = 1000000;
-        double k = 0.1;
         ll tryCount = 0;
 
         while (currentTime < TIME_LIMIT) {
-            double remainTime = TIME_LIMIT - currentTime;
-            int i = xor128() % g_vertexCount;
-            Vertex ov = goodVertexList[i];
-            Vertex v = createRandomVertex();
+            int vertexId = xor128() % g_vertexCount;
+            Vertex ov = bestVertexList[vertexId];
+            COORD c = createRandomCoord();
+
             assert(g_field[ov.y][ov.x]);
             g_field[ov.y][ov.x] = false;
-            assert(!g_field[v.y][v.x]);
-            g_field[v.y][v.x] = true;
-            goodVertexList[i] = v;
-            double score = calcScore(goodVertexList);
-            diffScore = bestScore - score;
+            assert(!g_field[c.y][c.x]);
+            g_field[c.y][c.x] = true;
 
-            if (bestScore < score) {
-                bestScore = score;
-                bestVertexList = goodVertexList;
-            }
+            bestVertexList[vertexId].y = c.y;
+            bestVertexList[vertexId].x = c.x;
+            Diff d = calcVertexDiff(bestVertexList, vertexId);
+            double diff = bestValue + (d.range - ov.range());
 
-            if (goodScore < score || (xor128() % R < R * exp(-diffScore / (k * remainTime)))) {
-                goodScore = score;
+            if (bestValue > diff) {
+                bestValue = diff;
+                bestVertexList[vertexId].minDiff = d.min;
+                bestVertexList[vertexId].maxDiff = d.max;
             } else {
                 assert(!g_field[ov.y][ov.x]);
                 g_field[ov.y][ov.x] = true;
-                assert(g_field[v.y][v.x]);
-                g_field[v.y][v.x] = false;
-                goodVertexList[i] = ov;
+                assert(g_field[c.y][c.x]);
+                g_field[c.y][c.x] = false;
+                bestVertexList[vertexId].y = ov.y;
+                bestVertexList[vertexId].x = ov.x;
             }
 
             currentTime = getTime(g_startCycle);
@@ -163,7 +222,7 @@ public:
         return bestVertexList;
     }
 
-    Vertex createRandomVertex() {
+    COORD createRandomCoord() {
         int y, x;
 
         do {
@@ -171,7 +230,7 @@ public:
             x = xor128() % FIELD_SIZE;
         } while (g_field[y][x]);
 
-        return Vertex(y, x);
+        return COORD(y, x);
     }
 
     vector<int> createAnswer(vector <Vertex> &vertexList) {
@@ -186,22 +245,38 @@ public:
         return ret;
     }
 
-    double calcScore(vector <Vertex> &vertexList) {
+    Node calcScore(vector <Vertex> &vertexList) {
         double minDiff = DBL_MAX;
         double maxDiff = 0.0;
+        double totalRange = 0.0;
 
-        for (int i = 0; i < g_edgeCount; i++) {
-            Edge edge = g_edgeList[i];
-            Vertex v1 = vertexList[edge.from];
-            Vertex v2 = vertexList[edge.to];
-            double dist = calcDist(v1, v2);
+        for (int i = 0; i < g_vertexCount; i++) {
+            Vertex *v = &vertexList[i];
+            minDiff = min(minDiff, v->minDiff);
+            maxDiff = max(maxDiff, v->maxDiff);
+            totalRange += v->range();
+        }
 
-            double diff = dist / (double) edge.dist;
+        return Node(totalRange, maxDiff, minDiff);
+    }
+
+    Diff calcVertexDiff(vector <Vertex> &vertexList, int fromId) {
+        double minDiff = DBL_MAX;
+        double maxDiff = 0.0;
+        Vertex from = vertexList[fromId];
+        int nsize = from.neighbors.size();
+
+        for (int j = 0; j < nsize; j++) {
+            int toId = from.neighbors[j];
+            Vertex to = vertexList[toId];
+            double dist = calcDist(from, to);
+            assert(g_desireDist[fromId][toId] > 0);
+            double diff = dist / (double) g_desireDist[fromId][toId];
             minDiff = min(minDiff, diff);
             maxDiff = max(maxDiff, diff);
         }
 
-        return minDiff / maxDiff;
+        return Diff(minDiff, maxDiff, maxDiff - minDiff);
     }
 
     inline double calcDist(Vertex &v1, Vertex &v2) {
@@ -216,11 +291,14 @@ public:
 // -------8<------- end of solution submitted to the website -------8<-------
 template<class T>
 void getVector(vector <T> &v) { for (int i = 0; i < v.size(); ++i) cin >> v[i]; }
+
 int main() {
     TIME_LIMIT = 1.0;
     GraphDrawing gd;
-    int N; cin >> N;
-    int E; cin >> E;
+    int N;
+    cin >> N;
+    int E;
+    cin >> E;
     vector<int> edges(E);
     getVector(edges);
     vector<int> ret = gd.plot(N, edges);
